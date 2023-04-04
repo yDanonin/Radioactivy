d8d6a87cfeb47898772a8e86d2b57c608d4a5c75
ydanonin <fabianorocha102003@gmail.com>
2023-04-04 18:12:00 -0300

    Initial test setup

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(1+1, 2);
+    }
+}
